1765dec55fdf092b8e9fafa325189a05917a2c75
Fiona Lapham <fionadlapham@gmail.com>
2018-06-11 12:07:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,13 @@ class Application
         resp.write "#{item}\n"
       end
     end
+  elsif req.path.match(/add/)
+      search_term = req.params["item"]
+       if @@items.each do |i|
+         i == search_term
+       end
+       @@cart << search_term
+     else
     else  
       resp.write "Path Not Found"
     end
