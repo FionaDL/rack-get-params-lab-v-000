03cb8a3c48197d71a0ba0bb23bbb7820c0d18cfb
Fiona Lapham <fionadlapham@gmail.com>
2018-06-11 12:14:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,6 +28,7 @@ class Application
          i == search_term
         end
        @@items << search_term
+       resp.write "added #{search_term}"
      else
        resp.write "We don't have that item."
      end
