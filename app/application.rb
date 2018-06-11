5b0aa9ca505184bdb9810255227b6f3bc1e29805
Fiona Lapham <fionadlapham@gmail.com>
2018-06-11 12:14:40 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
        if @@items.find do |i|
          i == search_term
         end
-       @@items << search_term
+       @@cart << search_term
        resp.write "added #{search_term}"
      else
        resp.write "We don't have that item."
